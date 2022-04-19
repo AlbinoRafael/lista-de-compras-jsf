8b4e4dd55668448885e492729e755236059a4204
Rafael Albino <fael10pg@gmail.com>
2022-04-19 13:30:25 -0300

    m2s8-ex8

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,7 +2,15 @@
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
-O
-src/main/webapp/WEB-INF/web.xml,2\e\2e22b6edd746fa3dc40ad7276c87c8d87d72a392
 X
-(src/main/webapp/WEB-INF/faces.config.xml,a\5\a5242211a81e23dbe8f5961cc940ec3b2a4b56fc
+(src/main/webapp/resources/css/styles.css,4\2\427775e2015b413542fec34801ef44d8d4e22b2c
+w
+Gsrc/main/java/br/senai/listadecomprasjsf/repository/ItemRepository.java,b\1\b1d62077fbe10beca29deba0bcef8b685e2d5d66
+{
+Ksrc/main/java/br/senai/listadecomprasjsf/validator/QuantidadeValidator.java,6\9\692f3dcdee90d09b1a346b519ce1e875e088848a
+X
+(src/main/webapp/WEB-INF/faces-config.xml,9\2\921ab541b29cd2aba50bbba38a0af79890ebe16a
+J
+src/main/webapp/index.html,1\3\1337d72a3b1928dba82aa5210f5cdc0e88ce895b
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/br/senai/listadecomprasjsf/model/ItemDeLista.java
+++ src/main/java/br/senai/listadecomprasjsf/model/ItemDeLista.java
@@ -1,17 +1,20 @@
 package br.senai.listadecomprasjsf.model;
 
+
+import javax.validation.constraints.NotEmpty;
 import java.util.Arrays;
 import java.util.List;
 
 public class ItemDeLista {
-
+    @NotEmpty(message = "{item.descricao.NotEmpty.message}")
     private String descricao;
 
     private Float quantidade;
 
     private String medida;
 
-    public ItemDeLista(){}
+    public ItemDeLista() {
+    }
 
     public ItemDeLista(String descricao, Float quantidade, String medida) {
         this.descricao = descricao;
@@ -42,12 +45,13 @@ public class ItemDeLista {
     public void setMedida(String medida) {
         this.medida = medida;
     }
+
     public List<String> getMedidas() {
         return Arrays.asList("unidade(s)", "kilo(s)", "litro(s)", "garrafa(s)", "pacote(s)");
     }
 
     @Override
     public String toString() {
-        return quantidade+" "+medida +" de "+descricao;
+        return quantidade + " " + medida + " de " + descricao;
     }
 }
